adfb3c9bb09a37fa014b5df2a621cf7fce1d5209
danialaswad <azswad20@gmail.com>
2014-11-28 00:26:25 +0100

    new design
    version 1.0

---
+++ template/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>R&amp;TM</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="menu">
+			<?php  include ('menu.php'); ?>
+		</div>
+		<div id="logo">
+			<h1><a href="#">R&T Malaisie</a></h1>
+			<span></span> </div>
+	</div>
+</div>
+<div id="wrapper1">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2>Bienvenue sur le site du R&amp;T:Malaisie</h2>
+			<span class="byline"></span> </div>
+		<div class="content">
+			<p></p>
+		</div>
+	</div>
+</div>
+<div id="wrapper3">
+	<div id="wrapper-blog" class="container">
+		<div id="post">
+			<h1>ALBUM</h1><br>
+			<div class="post"> <span class="date">Juin 20, 2014</span> <img src="images/carras.jpg" class="image image-full" />
+				<h2><strong>Pique-nique à carras</strong></h2>
+				<p>Le 20 juin 2014, nous avons fait un pique-nique &aacute; Carras afin d'accueillir les nouveaux arrivants les PF11.</p>
+
+			</div>
+			<div class="post"> <span class="date">Novembre 16, 2013</span> <img src="images/sortiecheval.jpg" class="image image-full" />
+				<h2><strong>Sortie cheval</strong></h2>
+				<p>Le 16 novembre 2013, nous sommes all&eacute;s &agrave; Sainte Agn&egrave;s pr&egrave;s de Menton pour faire du cheval.</p>
+			</div>
+			<div class="post no-border"> <span class="date">Juin 17, 2013</span> <img src="images/marineland.png" alt="" class="image image-full" />
+				<h2><strong>Sortie &agrave; Marineland</strong></h2>
+				<p>Le 17 juin 2013, nous avons fait une excursion avec les autres &eacute;tudiants malaisiens de l'IUT GEII &agrave; Marineland.</p>
+			</div>
+		</div>
+		<div id="list">
+			<div id="lbox1">
+				<ul class="style1">
+					
+				</ul>
+			</div>
+			<div id="lbox2">
+				<h2></h2>
+				<ul class="style2">
+					
+				</ul>
+			</div>
+			<div id="lbox2">
+				<h2></h2>
+				<ul class="style2">
+					
+				</ul>
+			</div>
+			<div id="lbox2">
+				<h2>Posuere augue amet</h2>
+				<ul class="style2">
+					<li><a href="#">Semper mod quis eget mi dolore</a></li>
+					<li><a href="#">Quam turpis feugiat sit dolor</a></li>
+					<li><a href="#">Amet ornare in hendrerit in lectus</a></li>
+					<li><a href="#">Consequat etiam lorem phasellus</a></li>
+					<li><a href="#">Amet ornare in hendrerit in lectus</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="wrapper2">
+	<div id="newsletter" class="container">
+		<div class="title">
+			<h2>Liens Externes</h2>
+				<ul class="style2">
+					<li>IUT Nice Côte d’azur<br/>
+				D&eacute;partement R&eacute;seaux et T&eacute;l&eacute;communications<br/>
+				650, Route des Colles, 06560 Valbonne<br/>
+				Adresse &eacute;lectronique : rt@iutsoph.unice.fr<br/>
+				Adresse web : <a href="http://rt.unice.fr/" target="_blank">http://rt.unice.fr/</a></li>
+					<li>Universiti Kuala Lumpur Malaysia France Institut<br/>
+				Section 14, Jalan Teras Jernang<br/>
+				43650 Bandar Baru Bangi, Selangor<br/>
+				Adresse web : <a href="http://www.unikl.edu.my/web/unikl-mfi/home" target="_blank">http://www.unikl.edu.my/web/unikl-mfi/home</a></li>
+					<li>Campus France<br>
+			Patent House, 144-02, 2nd Floor, Jalan Bukit Bintang<br>
+			Kuala Lumpur, 55100, Malaysia<br>
+			Telephone: +60 (0) 3- 2142 7475<br>
+			Fax: +60 (0) 3- 2142 7476<br>
+			Adresse web : <a href ="http://www.malaisie.campusfrance.org/en/" target="_blank">http://www.malaisie.campusfrance.org/en/</a></li>
+					
+				</ul>
+			<h2>Newsletter Signup</h2>
+			<span class="byline"></span> </div>
+		<div class="content">
+			<form method="post" action="#">
+				<div class="row half">
+					<div class="6u">
+						<input type="text" class="text" name="name" placeholder="Name" />
+					</div>
+					<div class="6u">
+						<input type="text" class="text" name="email" placeholder="Email" />
+					</div>
+				</div>
+				<div class="row half">
+					<div class="12u">
+						<textarea name="message" placeholder="Message"></textarea>
+					</div>
+				</div>
+				<div class="row">
+					<div class="12u"> <a href="#" class="button submit">Send Message</a> </div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<div id="copyright">
+	<p>&copy; 2013 R&amp;TM. All Rights Reserved.</p>
+</div>
+</body>
+</html>
---
+++ template/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+echo	"<ul>
+				<li class='current_page_item'><a href='#' accesskey='1' title=''>ACCUEIL</a></li>
+			<li><a href='Formations.php'accesskey='2' title=''>FORMATIONS</a></li>
+			<li><a href='Parcours.php'accesskey='3' title=''>PARCOURS</a></li>
+			<li><a href='Campus_France.php'accesskey='4' title=''>CAMPUS FRANCE</a></li>
+			<li><a accesskey='5' title=''>VIE &Eacute;TUDIANTE</a></li>
+			<li><a href='GeiiRnt.php'accesskey='7' title=''>GEII ET R&amp;T</a></li>
+			</ul>"
+
+
+
+?>
