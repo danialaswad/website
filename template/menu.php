ae8f9db3d07767c16c3d43c4f6d689a9a0ecd0aa
danialaswad <azswad20@gmail.com>
2014-12-05 20:40:43 +0100

    minor changes
    sidebar point tu #welcome id in the page

--- template/menu.php
+++ template/menu.php
@@ -1,9 +1,9 @@
 <?php
 
 echo	"
-			<li><a href='index.php' accesskey='1' title=''>ACCUEIL</a></li>
-			<li><a href='formation.php' accesskey='2' title=''>FORMATIONS</a></li>
-			<li><a href='parcours.php' accesskey='3' title=''>PARCOURS</a></li>
+			<li><a href='index.php#welcome' accesskey='1' title=''>ACCUEIL</a></li>
+			<li><a href='formation.php#welcome' accesskey='2' title=''>FORMATIONS</a></li>
+			<li><a href='parcours.php#welcome' accesskey='3' title=''>PARCOURS</a></li>
 			<li><a href='#' accesskey='4' title=''>CAMPUS FRANCE</a></li>
 			<li><a href='#' accesskey='5' title=''>VIE &Eacute;TUDIANTE</a></li>
 			<li><a href='#' accesskey='7' title=''>GEII ET R&amp;T</a></li>
