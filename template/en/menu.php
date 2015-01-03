566ae04a6e92389ce16613ce1e106a8ae70f746e
danialaswad <azswad20@gmail.com>
2015-01-03 02:27:03 +0100

    test

---
+++ template/en/menu.php
@@ -0,0 +1,17 @@
+<?php
+
+echo	"
+			<li><a href='index.php#welcome' accesskey='1' title=''>HOME</a></li>
+			<li><a href='formation.php#welcome' accesskey='2' title=''>PROGRAMMES</a></li>
+			<li><a href='parcours.php#welcome' accesskey='3' title=''>ADMISSION</a></li>
+			<li><a href='campusfrance.php#welcome' accesskey='4' title=''>CAMPUS FRANCE</a></li>
+			<li><a href='vietudiant.php#welcome' accesskey='5' title=''>STUDENT LIFE</a></li>
+			<li><a href='geiirt.php#welcome' accesskey='7' title=''>GEII AND R&amp;T</a></li>
+			<li><a href='album.php#welcome' accesskey='8' title=''>ALBUM</a></li>
+			<li><a href='#' accesskey='9' title=''>FORUM</a></li>
+			"
+
+
+
+?>
+
